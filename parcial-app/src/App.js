6312d60a9bbab49165e67f6afb7eaa9e92b8f865
parcialUsuario <pacolucasmargarita@gmail.com>
2024-10-16 13:20:13 -0500

    practica

---
+++ parcial-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import js from "./assets/jss.png"
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>parcial</h1>
+        <img src={js}></img>
+      </header>
+    </div>
+  );
+}
+
+export default App;
